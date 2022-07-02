1a7a71b78f8e88114e5ca82deb3bf4745ca90f3c
Jared Rand <jjrand96@gmail.com>
2022-07-02 13:00:03 -0400

    Add logging to upload file

--- upload.sh
+++ upload.sh
@@ -1,4 +1,7 @@
+echo Cleaning out the 'dist' folder on remote, please authenticate...
 # Clean out the existing `dist` folder
 ssh jaredrand.ca -t rm -i -f -- website/dist/*
+echo Cleanup complete.
+echo Please re-login to transfer documents.
 # Upload the documents in the current `dist` folder
 scp -r dist jaredrand.ca:website
